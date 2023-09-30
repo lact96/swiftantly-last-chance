5c660e401f54c9319cb1e49ccdd9540c1c87a813
paltinum <58859233+paltinum@users.noreply.github.com>
2023-09-30 16:43:40 -0600

    second commit

---
+++ warm_emails/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WarmEmailsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'warm_emails'
---
+++ warm_up_bots/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WarmUpBotsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'warm_up_bots'
